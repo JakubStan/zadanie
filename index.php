1b3ee11759beb4366021deef8c8f4f4e9bd8454b
Jakub <stankiewiczj88nt@gmail.com>
2022-01-19 13:48:09 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,314 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+
+    <meta name="description" content=".....">
+    <meta name="keywords" content="......">
+    <meta name="author" content="Jakub">
+
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+    <title>ZADANIE REKRUTACYJNE</title>
+
+    <link rel="icon" href="img/icons/favicon.ico" type="image/x-icon">
+
+</head>
+<body>
+
+
+<main>
+
+    <h3>Obszary naszych kompetencji</h3>
+    <p>Pomagamy uczyć się na błędach innych. Wspieramy i rekomendujemy</p>
+
+<div class="abauth">
+
+<div class="circle-wrapper"> 
+
+    <a href="#" target="_blank" rel=”noopener” class="center-ikon">
+        <img src="img/icons/center_icon.png" loading="lazy" alt="">
+    </a>
+    
+
+   <div class="circle deg-0">
+        <a href="#" target="_blank" rel=”noopener”>
+            <img class="img-bg" src="img/icons/4.png" loading="lazy" alt="">
+            <p class="pos-1">Prawo międzynarodowe</p>
+        </a>        
+   </div> 
+
+   <div class="circle deg-30">
+   <a href="#" target="_blank" rel=”noopener”>
+            <img class="img-bg" src="img/icons/5.png" loading="lazy" alt="">
+            <p class="pos-1">Finansowanie handlu międzynarodowego</p>
+        </a>
+   </div> 
+
+   <div class="circle deg-60">
+   <a href="#" target="_blank" rel=”noopener”>
+            <img class="img-bg" src="img/icons/6.png" loading="lazy" alt="">
+            <p class="pos-1">Pozyskiwanie inwestorów (M&A)</p>
+        </a>
+   </div>
+
+   <div class="circle deg-90">
+        <a href="#" target="_blank" rel=”noopener”>
+            <img class="img-bg" src="img/icons/7.png" loading="lazy" alt="">
+            <p class="pos-2" >Nieruchomości</p>
+        </a>
+   </div>
+
+   <div class="circle deg-120">
+        <a href="#">
+            <img class="img-bg" src="img/icons/8.png" loading="lazy" alt="">
+            <p class="pos-3" >Komercjaizacja własności intelektualnej</p>
+        </a>
+   </div>
+
+   <div class="circle deg-150">
+        <a href="#" target="_blank" rel=”noopener”>
+            <img class="img-bg" src="img/icons/9.png" loading="lazy" alt="">
+            <p class="pos-4" >Podnoszenie produktywności</p>
+        </a>
+   </div>
+    
+   <div class="circle deg-180">
+        <a href="#" target="_blank" rel=”noopener”>
+            <img class="img-bg" src="img/icons/10.png" loading="lazy" alt="">
+            <p class="pos-5" >Crowdfunding</p>
+        </a>
+   </div>
+
+   <div class="circle deg-210">
+        <a href="#" target="_blank" rel=”noopener”>
+            <img class="img-bg" src="img/icons/11.png" loading="lazy" alt="">
+            <p class="pos-7" >Ubezpieczenia międzynarodowe</p>
+        </a>
+   </div>
+
+   <div class="circle deg-240">
+        <a href="#" target="_blank" rel=”noopener”>
+            <img class="img-bg" src="img/icons/12.png" loading="lazy" alt="">
+            <p class="pos-5" >Marketing 360<sup>0</sup></p>
+        </a>
+   </div>
+
+   <div class="circle deg-270">
+        <a href="#" target="_blank" rel=”noopener”>
+            <img class="img-bg" src="img/icons/1.png" loading="lazy" alt="">
+            <p class="pos-6" >Zarządzanie projektami</p>
+        </a>
+   </div>
+
+   <div class="circle deg-300">
+        <a href="#" target="_blank" rel=”noopener”>
+            <img class="img-bg" src="img/icons/2.png" loading="lazy" alt="">
+            <p class="pos-1" >Dotacje i granty międzynarodowe</p>
+        </a>
+   </div>
+
+   <div class="circle deg-330">
+        <a href="#" target="_blank" rel=”noopener”>
+            <img class="img-bg" src="img/icons/3.png" loading="lazy" alt="">
+            <p class="pos-1" >Międznarodowa księdowość, kadry i podatki</p>
+        </a>  
+   </div>
+
+  </div>  
+
+
+    <div class="mobile-content">
+
+        <a target="_blank" rel=”noopener” href="#">
+            <img src="img\icons\center_icon.png" loading="lazy" alt=""> 
+        </a>
+
+        <div class="list">
+
+            <a class="list-item" target="_blank" rel=”noopener” href="#">
+                <img src="img\icons\1.png"loading="lazy" alt="">
+                <p>Zarządzanie projektami</p>
+            </a>
+
+            <a class="list-item" target="_blank" rel=”noopener” href="#">
+                <img src="img\icons\2.png" loading="lazy" alt="">
+                <p>Dotacje i granty międzynarodowe</p>
+            </a>
+
+            <a class="list-item" target="_blank" rel=”noopener” href="#">
+                <img src="img\icons\3.png" loading="lazy" alt="">
+                <p>Międzynarodowa księgowosć, kadry i podatki</p>
+            </a>
+
+            <a class="list-item" target="_blank" rel=”noopener” href="#">
+                <img src="img\icons\4.png" loading="lazy" alt="">
+                <p>Prawo międzynarodowe</p>
+            </a>
+
+            <a class="list-item" target="_blank" rel=”noopener” href="#">
+                <img src="img\icons\5.png" loading="lazy" alt="">
+                <p>Finansowanie handlu międzynarodowego</p>
+            </a>
+
+            <a class="list-item" target="_blank" rel=”noopener” href="#">
+                <img src="img\icons\6.png" loading="lazy" alt="">
+                <p>Pozyskiwanie inwestorów (M&A)</p>
+            </a>
+
+            <a class="list-item" target="_blank" rel=”noopener” href="#">
+                <img src="img\icons\7.png" loading="lazy" alt="">
+                <p>Nieruchomości</p>
+            </a>
+
+            <a class="list-item" target="_blank" rel=”noopener” href="#">
+                <img src="img\icons\8.png" loading="lazy" alt="">
+                <p>Komercjalizacja własności intelektualnej</p>
+            </a>
+
+            <a class="list-item" target="_blank" rel=”noopener” href="#">
+                <img src="img\icons\9.png" loading="lazy" alt="">
+                <p>Podnoszenie produktywności</p>
+            </a>
+
+            <a class="list-item" target="_blank" rel=”noopener” href="#">
+                <img src="img\icons\10.png" loading="lazy" alt="">
+                <p>Crowdfunding</p>
+            </a>
+
+            <a class="list-item" target="_blank" rel=”noopener” href="#">
+                <img src="img\icons\11.png" loading="lazy" alt="">
+                <p>Ubezpieczenia międzynarodowe</p>
+            </a>
+
+            <a class="list-item" target="_blank" rel=”noopener” href="#">
+                <img src="img\icons\12.png" loading="lazy" alt="">
+                <p>Marketing 360<sup>0</sup></p>
+            </a>
+
+        </div> <!-- End of list div -->
+
+    </div> <!-- End of mobile-content div -->
+
+</div>
+
+
+</main>
+
+
+
+<footer>
+
+    <h3>Skontaktuj się z nami</h3>
+    
+    <span>
+        Zadzwoń <a href="tel:+48535015835">+48 535 015 835</a> lub wypełnij formularz.
+    </span>
+
+    <form action="index.php" method="post">
+
+        <div class="container">
+
+                <div class="row">            
+                        <div class="col-md-4"><input type="text" class="form-control" id="name" placeholder="Imię i Nazwisko" value="<?php if(isset($_POST['name'])) echo $_POST['name'];?>" name="name" required></div>
+                        <div class="col-md-4"><input type="tel"  class="form-control" id="phone" placeholder="Telefon" value="<?php if(isset($_POST['phone'])) echo $_POST['phone'];?>" name="phone" required></div>
+                        <div class="col-md-4"><input type="email" class="form-control" id="email" placeholder="E-mail" value="<?php if(isset($_POST['email'])) echo $_POST['email'];?>" name="email" required></div>         
+                </div> <!-- End of first row -->
+
+                <div class="row">
+                        <div class="col-md-4"><input type="text" class="form-control" id="company" placeholder="Firma" value="<?php if(isset($_POST['company'])) echo $_POST['company'];?>" name="company" required></div>
+                                
+                        <div class="col-md-4">
+                            <div class="selectdiv">               
+                            <select class="form-select" name="workers" id="workers" >
+                                <option value="" disabled selected>Ilość pracowników</option>
+                                <option>0-10</option>
+                                <option>10-30</option>
+                                <option>30-60</option>
+                                <option>60-100</option>
+                            </select>                
+                        </div><!-- End of selectdiv div -->
+                        </div><!-- End of col div -->
+
+                        <div class="col-md-4">
+                        <div class="selectdiv">               
+                            <select class="form-select" name="income"  id="income">
+                                <option value="" disabled selected>Roczny dochód firmy</option>
+                                <option>10 tyś</option>
+                                <option>10-30 tyś</option>
+                                <option>30-60 tyś</option>
+                                <option>60-100 tyś</option>
+                            </select>                
+                            </div><!-- End of selectdiv div -->
+                        </div><!-- End of col div -->
+
+                </div> <!-- End of second row -->
+
+                <div class="row">
+                    <div class="col-md-12">
+                    <textarea class="form-control" id="message" placeholder="Wiadomość" name="message"  required  value="<?php if(isset($_POST['message'])) echo $_POST['message'];?>" rows="5"></textarea>
+                    </div>    
+                </div><!-- End of third row -->
+
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="form-check" >
+                            <input class="form-check-input" type="checkbox" required  name="checkbox" value="" id="flexCheckDefault" >
+                            <label class="form-check-label" for="flexCheckDefault">
+                            Zapoznałem(-łam) się z 
+                            <a href="#" target="_blank" rel=”noopener”>Polityką prywatności</a>
+                            oraz z 
+                            <a href="#" target="_blank" rel=”noopener” >Zastrzeżeniami prawnymi</a>
+                            </label>
+                        </div>
+                    </div>
+                </div><!-- End of fourth row -->
+                
+            <button type="submit" name="submit" class="btn btn-form">WYŚLIJ</button>
+
+        </div><!-- End of container-->
+    
+    </form>
+
+<?php if(isset($_POST['submit'])) {    
+   $errors = array();
+   $success = null;
+   
+   
+    if(empty($errors)) {
+         $to = "__email__";
+         $company = $_POST['company'];    
+         $name_field = $_POST['name'];
+         $email_field = $_POST['email'];
+         $message = $_POST['message'];
+         $workers = $_POST['workers'];
+         $income = $_POST['income'];
+      
+        
+         $body = "From: $name_field\n Company: $company\n E-Mail: $email_field\n Message:\n $message";
+         $success = mail($to,"Wiadomość z formularza",  $body);
+    }
+    
+    if($success) {
+              echo "<p><strong>Dziękujemy za wiadomosć! Odpowiemy tak szybko jak to możliwe!</strong></p>";
+         } else {
+              echo "<div><h4 style='color:#b00e0e;' >Błąd wysłania wiadmości!</h4></div>";
+         }
+    }
+
+   ?>
+
+
+</footer>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script src="script.js"></script>
+
+</body>
+</html>
+
+
